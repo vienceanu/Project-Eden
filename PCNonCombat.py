249b43612656c1e36b366f1e3f1fb7a42dcca1b9
vienceanu <vladienceanu@gmail.com>
2022-04-30 18:00:17 -0400

    Added Ship Class

--- PCNonCombat.py
+++ PCNonCombat.py
@@ -1,6 +1,8 @@
 class PCNonCombat:
+    #type is made to indicate class, Haven't figure out how yet. 
     type = 0
     money = 50
     item_inventory = ["Laser Sword", "Laser Pistol"]
+    actions =["Speak", "Trade", "Ask"]
     
     
---
+++ Ship.py
@@ -0,0 +1,8 @@
+class Ship:
+    Inventory =[]
+    Fuel = 100
+    Passengers = []
+    #Dock status, if docked 1 else 0
+    dockStatus = 1
+    
+    
