98fc9a4d8d22b4683d0752e35f4318e710bad73b
vienceanu <vladienceanu@gmail.com>
2022-04-30 17:15:44 -0400

    Made help file for instructions & basic menu

---
+++ main.py
@@ -0,0 +1,31 @@
+#Main page, this is where we import from other files
+#Import Packages
+
+
+
+##########################################################
+#Objects
+
+##########################################################
+
+
+#Query text line
+exit == False
+val = ""
+def intro():
+    print("Welcome to Project Eden, A space Colony Text ADventure Simulator")
+    while True:
+        val = input("Welcome to the main menu!" )
+        print("\n")
+        if val == 'Exit' or val == 'exit':
+            quit()
+        elif val =='help' or val =='Help':
+            help_file = open("help.txt")
+            file_contents = help_file.read()
+            print(file_contents)
+        else:
+            print("Command not recognized")        
+
+intro()
+
+    
