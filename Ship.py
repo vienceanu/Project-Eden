4f0b09e23ab494c4befcd13dec022a5ec629303b
vienceanu <vladienceanu@gmail.com>
2022-04-30 18:14:19 -0400

    Finished Ship Class

--- Ship.py
+++ Ship.py
@@ -4,5 +4,7 @@ class Ship:
     Passengers = []
     #Dock status, if docked 1 else 0
     dockStatus = 1
+    #For resources need a dictionary
+    Resources = {"Lithium": 0, "Gold": 0, "Water": 0}
     
     
