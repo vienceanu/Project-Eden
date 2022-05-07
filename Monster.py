7ea28c826a963a327ecf0089cf00f47d20e8c4f7
vienceanu <vladienceanu@gmail.com>
2022-05-07 12:44:47 -0400

    changed monster

--- Monster.py
+++ Monster.py
@@ -1,4 +1,16 @@
-class Monster:
+class Pirate:
     Hp = 20
     DMG = 2
+    
+class Pirate_Captain:
+    Hp = 50
+    Dmg = 3
+    
+class Alien:
+    Hp = 30
+    Dmg = 5
+
+class Alien_Queen:
+    Hp = 100
+    Dmg = 10
     
