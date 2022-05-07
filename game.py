c9d814a04f23875d486b50d059fb2270b08cea14
vienceanu <vladienceanu@gmail.com>
2022-05-06 21:33:16 -0400

    formatting

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ exit = False
 def start():
     name = input("What is your name, cadet?\n")
     print("Greetings, " + name + ". Let's throw you in")
-    print("barely clinking to life, all of your alarms are going off, no oxygen, no hope")
+    print("barely clinging to life, all of your alarms are going off, no oxygen, no hope")
     print("How do you proceed?\n")
     val = input("Your Answer: ")
     
