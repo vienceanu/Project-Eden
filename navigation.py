b48403dbf50385a27a8b120245df572bc6d3d279
vienceanu <vladienceanu@gmail.com>
2022-05-18 21:55:39 -0400

    navigation verbs set up

--- navigation.py
+++ navigation.py
@@ -2,8 +2,9 @@ from Ship import Ship as Ship
 
 #Data 
 verbs = ["map", "travel", "survey", "mine",  ]
+solar_system = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto" ]
 
-
-while Ship.dockStatus == 0:
-    print("You drift Motionless through space")
+def navigation_mode():
+    while Ship.dockStatus == 0:
+        print("You drift Motionless through space")
     
