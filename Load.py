2fc6e6f09dfc36a1a76f9507ef734d0f8ddeebf7
Huiran <38442664+nala00033@users.noreply.github.com>
2022-06-11 12:10:16 -0400

    Create Load.py

---
+++ Load.py
@@ -0,0 +1,17 @@
+import json
+from Ship import Ship
+from trader import Trader
+
+with open("data.json", "r") as f:
+    data = json.load(f)
+
+Ship.Inventory = data['ship']['Inventory']
+Ship.Hull = int(data['ship']['Hull'])
+Ship.HP = Ship.Hull * int(data['ship']['HP'])
+Ship.Fuel = int(data['ship']['Fuel'])
+Ship.location = data['ship']['location']
+Ship.Passengers = data['ship']['Passengers']
+Ship.doge = int(data['ship']['Doge'])
+Ship.dockStatus = int(data['ship']['DockStatus'])
+Ship.Resources = data['ship']['Resources']
+Trader.money = data['money']
