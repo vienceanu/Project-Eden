56e2ed253a0afcdbdee5182de6a310bd1ddcbb08
2am2 <17.amar1@gmail.com>
2022-06-11 18:12:05 -0400

    added self tags added getHp for combat

--- Ship.py
+++ Ship.py
@@ -1,13 +1,16 @@
 class Ship:
-    Inventory ={"LG": 2}
-    Hull = 1.2
-    HP = 50 * Hull
-    Fuel = 100
-    location = "Pluto"
-    Passengers = []
-    #Dock status, if docked 1 else 0
-    dockStatus = 0
-    #For resources need a dictionary
-    Resources = {"Lithium": 0, "Titanium": 0, "Iron": 0, "Uranium": 0, "Ore-X": 0, "Torpedo": 0}
+    def __init__(self):
+        self.Inventory ={"LG": 2}
+        self.Hull = 1.2
+        self.maxHP = 50 * self.Hull
+        self.HP = self.maxHP
+        self.Fuel = 100
+        self.location = "Pluto"
+        self.Passengers = []
+        #Dock status, if docked 1 else 0
+        self.dockStatus = 0
+        #For resources need a dictionary
+        self.Resources = {"Lithium": 0, "Titanium": 0, "Iron": 0, "Uranium": 0, "Ore-X": 0, "Torpedo": 0}
     
-    
+    def getHp(self):
+        return self.HP
